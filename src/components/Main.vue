49bc9ab229103eb33b01dccec4c4e428f111ca5e
raskardev <alexcdev95@gmail.com>
2020-03-09 18:46:13 +0100

    wip(): create Main component

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="flex overflow-x-auto py-8 px-2 bg-body">
+    <TasksList name="Prueba" />
+  </main>
+</template>
+
+<script>
+import TasksList from './TasksList.vue';
+
+export default {
+  name: 'Main',
+  components: {
+    TasksList,
+  },
+};
+</script>
+
+<style scoped>
+main {
+  height: calc(100% - 48px);
+}
+</style>
