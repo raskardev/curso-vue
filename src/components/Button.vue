b82c476a34269faf35f519edec964cd6c53d1c8a
raskardev <alexcdev95@gmail.com>
2020-03-09 18:44:46 +0100

    feat(): create Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button hover:bg-button-hovered">
+    <i :class="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
