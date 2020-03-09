34854f6eb90dd6879e76b4338b67a0e9c811a5ed
raskardev <alexcdev95@gmail.com>
2020-03-09 23:11:20 +0100

    chore(): add TaskCard to TasksList

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="flex-shrink-0 w-68 min-h-16 overflow-y-auto mr-2 ml-2 rounded bg-gray-400"></div>
+  <div class="flex-shrink-0 w-68 h-fit mr-2 ml-2 p-2 rounded bg-gray-400">
+    <h3 class="pl-3 mb-2 text-base text-black font-medium">{{name}}</h3>
+    <div id="tasks" class="overflow-y-auto scrolling-touch">
+      <TaskCard />
+      <TaskCard />
+      <TaskCard />
+      <TaskCard />
+      <TaskCard />
+      <TaskCard />
+      <TaskCard />
+    </div>
+  </div>
 </template>
 
 <script>
+import TaskCard from './TaskCard.vue';
+
 export default {
   name: 'TasksList',
+  components: {
+    TaskCard,
+  },
   props: {
     name: {
       type: String,
@@ -18,8 +34,18 @@ export default {
 };
 </script>
 
-<style scoped>
-div {
-  height: fit-content;
+<style>
+#tasks {
+  max-height: 36rem;
+}
+@media (max-width: 768px) {
+  #tasks {
+    max-height: 55rem;
+  }
+}
+@media (max-width: 360px) {
+  #tasks {
+    max-height: 31rem;
+  }
 }
 </style>
