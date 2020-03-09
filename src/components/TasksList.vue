0e8f1d923047b4b342c3501ed11adf4c07cc1e73
raskardev <alexcdev95@gmail.com>
2020-03-09 18:46:02 +0100

    wip(): create TasksList component

---
+++ src/components/TasksList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-68 overflow-y-auto rounded bg-gray-400"></div>
+</template>
+
+<script>
+export default {
+  name: 'TasksList',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    tasks: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
